dff208d7f19248db65af9d940e7ad82f03099c4d
Bara Nur Imaduddin <baraanur03@gmail.com>
2026-04-14 10:29:42 +0700

    Player Movement TopDown Style
    Animation still require more idle state and movement still to fast. Lastly the chat is small af

---
+++ Assets/Script/Sidiq/TopDownPlayerMovement.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class TopDownPlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private Animator animator;
+
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        // Normalize diagonal movement so it's not faster than axis-aligned
+        if (movement.sqrMagnitude > 1f)
+            movement = movement.normalized;
+
+        bool isMoving = movement.sqrMagnitude > 0.01f;
+        animator.SetBool("isMoving", isMoving);
+
+        if (isMoving)
+        {
+            animator.SetFloat("moveX", movement.x);
+            animator.SetFloat("moveY", movement.y);
+        }
+        // When idle: leave moveX/moveY at last values so blend tree holds last facing direction
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = movement * speed;
+    }
+}
