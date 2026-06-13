6a8a2bd253ae87b8a7324c05f225ca9008513e01
Cathzer <stcaptainz1309@gmail.com>
2026-06-13 13:56:32 +0700

    Ensure desktop summaries return to menu
    Add a self-contained summary return handler that restores normal time scale and loads All_Menu. Remove legacy scene-manager button listeners from all main topic desktop summaries.

---
+++ Assets/!Script/Sidiq/SummaryReturnToMenu.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class SummaryReturnToMenu : MonoBehaviour
+{
+    private const string MenuSceneName = "All_Menu";
+
+    private Button _button;
+    private bool _isLoading;
+
+    private void Awake()
+    {
+        _button = GetComponent<Button>();
+        _button.onClick.AddListener(ReturnToMenu);
+    }
+
+    private void OnDestroy()
+    {
+        if (_button != null)
+            _button.onClick.RemoveListener(ReturnToMenu);
+    }
+
+    public void ReturnToMenu()
+    {
+        if (_isLoading)
+            return;
+
+        _isLoading = true;
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(MenuSceneName);
+    }
+}
